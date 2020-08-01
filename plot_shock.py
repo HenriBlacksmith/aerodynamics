f3729390a7fef179add69e2ae28e89622cd1cb0d
HenriBlacksmith <raid94@hotmail.fr>
2020-08-01 21:38:55 +0200

    Refactoring code

---
+++ plot_shock.py
@@ -0,0 +1,54 @@
+from numpy import tan, array
+from matplotlib.pyplot import plot, xlabel, ylabel, title, show, rc, gca, savefig, subplots, close, arrow, annotate
+
+# Plotting functions
+def wall_points(theta):
+    x = array([-1., 0., 1.])
+    y = array([0., 0., tan(theta)])
+    return array([x,y])
+
+def weak_shock_wave_points(sigma_weak):
+    x = array([0., 1.])
+    y = array([0., tan(sigma_weak)])
+    return array([x,y])
+
+def strong_shock_wave_points(sigma_strong):
+    x = array([0., 1.])
+    y = array([0., tan(sigma_strong)])
+    return array([x,y])
+
+def plot_shock(theta, sigma_weak, M, M1, ax):
+    [wall_x, wall_y] = wall_points(theta)
+    [weak_x, weak_y] = weak_shock_wave_points(sigma_weak)
+    ax.plot(wall_x, wall_y, 'r')
+    ax.plot(weak_x, weak_y, 'b')
+    # Arrow representing pre-shock velocity
+    ax.annotate("", 
+            xy=(-0.7, 0.3), 
+            xytext=(-1, 0.3), 
+            arrowprops=dict(arrowstyle="->"), 
+            horizontalalignment='left',
+            verticalalignment='bottom')
+    ax.annotate("M=" + str(M), 
+            xy=(-0.7, 0.35), 
+            xytext=(-1, 0.35))
+    # Arrow representing post-shock velocity
+    ax.annotate("", 
+            xy=(0.6, 0.35+0.2*tan(theta)), 
+            xytext=(0.4, 0.35), 
+            arrowprops=dict(arrowstyle="->"), 
+            horizontalalignment='left',
+            verticalalignment='bottom')
+    ax.annotate("$M_1=$" + str(round(M1,2)), 
+            xy=(0.6, 0.6+0.2*tan(theta)), 
+            xytext=(0.6, 0.4))
+    #ax.annotate("M_1=" + str(M1), 
+    #        xy=(0.6, 0.5), 
+    #        xytext=(0.4, 0.5))
+    rc('text', usetex=True)
+    gca().set_aspect('equal', adjustable='box')
+    xlabel('$x$')
+    ylabel('$y$')
+    title('Oblique shock')
+    savefig('shock.png', format='png')
+    close('all')
