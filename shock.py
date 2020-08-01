d2a22c14b228008f06b5cd9c7c9f262cba93a8d8
HenriBlacksmith <raid94@hotmail.fr>
2020-08-01 21:33:36 +0200

    Adding new values

--- shock.py
+++ shock.py
@@ -1,4 +1,4 @@
-from numpy import radians, sin, tan, cos, pi, arccos, arctan, array
+from numpy import  degrees, radians, sin, tan, cos, pi, arccos, arctan, array
 from matplotlib.pyplot import plot, xlabel, ylabel, title, show, rc, gca, savefig, subplots, close, arrow, annotate
 
 # Plotting functions
@@ -17,11 +17,12 @@ def strong_shock_wave_points(sigma_strong):
     y = array([0., tan(sigma_strong)])
     return array([x,y])
 
-def plot_shock(theta, sigma_weak, sigma_strong, ax):
+def plot_shock(theta, sigma_weak, M, M1, ax):
     [wall_x, wall_y] = wall_points(theta)
     [weak_x, weak_y] = weak_shock_wave_points(sigma_weak)
     ax.plot(wall_x, wall_y, 'r')
     ax.plot(weak_x, weak_y, 'b')
+    # Arrow representing pre-shock velocity
     ax.annotate("", 
             xy=(-0.7, 0.3), 
             xytext=(-1, 0.3), 
@@ -31,6 +32,19 @@ def plot_shock(theta, sigma_weak, sigma_strong, ax):
     ax.annotate("M=" + str(M), 
             xy=(-0.7, 0.35), 
             xytext=(-1, 0.35))
+    # Arrow representing post-shock velocity
+    ax.annotate("", 
+            xy=(0.6, 0.35+0.2*tan(theta)), 
+            xytext=(0.4, 0.35), 
+            arrowprops=dict(arrowstyle="->"), 
+            horizontalalignment='left',
+            verticalalignment='bottom')
+    ax.annotate("$M_1=$" + str(round(M1,2)), 
+            xy=(0.6, 0.6+0.2*tan(theta)), 
+            xytext=(0.6, 0.4))
+    #ax.annotate("M_1=" + str(M1), 
+    #        xy=(0.6, 0.5), 
+    #        xytext=(0.4, 0.5))
     rc('text', usetex=True)
     gca().set_aspect('equal', adjustable='box')
     xlabel('$x$')
@@ -57,8 +71,7 @@ E = (2./27.)*A**3-A*B/3.+C
 Delta = E**2 - 4*D**3
 
 print("=================================")
-print("theta=" + str(theta) + " rad")
-print("theta=" + str(theta_deg) + " deg")
+print("theta = " + str(theta) + " rad" + " = " + str(theta_deg) + " deg")
 print("_________________________________")
 print("A=" + str(A))
 print("B=" + str(B))
@@ -76,6 +89,20 @@ if (Delta < 0):
 
     sigma_weak = arctan(1./F_weak)
     sigma_strong = arctan(1./F_strong)
+    sigma = sigma_weak
+
+    Mn0 = M*sin(sigma) # Normal Mach (pre-shock)
+
+    # Ratios (post/pre shock)
+    p1_p0 = 2*gamma/(gamma+1)*Mn0**2-(gamma-1)/(gamma+1)
+    rho1_rho0 = 1/(2/((gamma+1)*Mn0**2)+(gamma-1)/(gamma+1))
+    T1_T0 = p1_p0/rho1_rho0
+    pi1_pi0 = p1_p0**(-1/(gamma-1))*rho1_rho0**(gamma/(gamma-1))
+
+    # Post-shock values
+    Mn1 =((1.+(gamma-1.)/2.*Mn0**2.)/(gamma*Mn0**2.-(gamma-1.)/2.))**(1./2.) # Normal Mach number (post-shock)
+    M1 = Mn1*sin(sigma-theta) # Mach number (post-shock)
+    Cp1 =(p1_p0-1)/(gamma/2*M**2) # Pressure coefficient (post-shock)
 
     print("_________________________________")
     print("Weak shock solution : ")
@@ -87,29 +114,30 @@ if (Delta < 0):
     print("Phi_strong=" + str(Phi_strong) + " rad")
     print("F_strong=" + str(F_strong))
     print("sigma_strong=" + str(sigma_strong) + " rad")
+    print("_________________________________")
+    print("Weak shock solution is chosen as physical solution : ")
+    print("sigma=" + str(sigma) + " rad = " + str(degrees(sigma)) + " deg")
+    print("Mn0=" + str(Mn0))
+    print("Mn1=" + str(Mn1))
+    print("M1=" + str(M1))
+    print("Cp1=" + str(Cp1))
+    print("_________________________________")
+    print("Common ratios : ")
+    print("p1_p0=" + str(p1_p0))
+    print("rho1_rho0=" + str(rho1_rho0))
+    print("T1_T0=" + str(T1_T0))
+    print("pi1_pi0=" + str(pi1_pi0))
     print("=================================")
     fig, ax = subplots(1,1)
-    plot_shock(theta, sigma_weak, sigma_strong, ax)
+    plot_shock(theta, sigma_weak, M, M1, ax)
+    
 else : 
     print("_________________________________")
     print("No shock")
     print("=================================")
-
 
 # Pre-shock ratios (Ideal homoentropic gas)
 # omega_bar0 = (1+(gamma-1)/2*M**2)**(-gamma/(gamma-1))
 # R0 =(1+(gamma-1)/2*M**2)**(-1/(gamma-1))
 # Theta0 =(1+(gamma-1)/2*M**2)**-1
 # Sigma0 =(2/(gamma+1))**((gamma+1)/(2*(gamma-1)))*1/Theta0*(1+(gamma-1)/2*M**2)**((gamma+1)/(2*(gamma-1)))
-
-# Ratios (post/pre shock)
-# p1_p0 = 2*gamma/(gamma+1)*Mn0**2-(gamma-1)/(gamma+1)
-# rho1_rho0 = 1/(2/((gamma+1)*Mn0**2)+(gamma-1)/(gamma+1))
-# T1_T0 = p1_p0/rho1_rho0
-# pi1_pi0 = p1_p0**(-1/(gamma-1))*rho1_rho0**(gamma/(gamma-1))
-
-# Post-shock values
-# Mn1 =((1+(gamma-1)/2*Mn0**2)/(gamma*Mn0**2-(gamma-1)/2))**(1/2) # Normal Mach number (post-shock)
-# M1 = Mn1*sin(sigma-theta) # Mach number (post-shock)
-# Cp1 =(p1_p0-1)/(gamma/2*M**2) # Pressure coefficient (post-shock)
-
