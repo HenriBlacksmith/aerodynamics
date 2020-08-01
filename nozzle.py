cecfdbd4b931dd86dc53674493486f6eeebd846c
HenriBlacksmith <raid94@hotmail.fr>
2020-08-01 23:22:17 +0200

    Adding code for nozzle computations

---
+++ nozzle.py
@@ -0,0 +1,9 @@
+ps = 1.0
+pi0 = 1.5
+
+# Situations occuring while increasing pi0
+# A - No Shock (one sonic section)
+# AB - Shock in divergent
+# B - Shock at the end of the divergent
+# BC - Oblique Shock at the end of the divergent
+# C - Fully sonic divergent
