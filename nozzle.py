c4e80a04b91dbca33524471333effd6e38e68244
HenriBlacksmith <raid94@hotmail.fr>
2020-08-01 23:34:19 +0200

    Adding nozzle plot

--- nozzle.py
+++ nozzle.py
@@ -1,5 +1,11 @@
+from numpy import linspace
+from matplotlib.pyplot import plot, xlabel, ylabel, title, show, rc, savefig, close, subplots, grid, gca
+
 ps = 1.0
 pi0 = 1.5
+
+def shape(x):
+    return 1 + 0.407*x**2*(3-2*x)
 
 # Situations occuring while increasing pi0
 # A - No Shock (one sonic section)
@@ -7,3 +13,19 @@ pi0 = 1.5
 # B - Shock at the end of the divergent
 # BC - Oblique Shock at the end of the divergent
 # C - Fully sonic divergent
+
+def plot_nozzle():
+    fig, ax = subplots(1,1)
+    geom_x = linspace(0., 1., 100)
+    geom_y = shape(geom_x)
+    ax.plot(geom_x, geom_y, 'r')
+    ax.plot(geom_x, -geom_y, 'r')
+    rc('text', usetex=True)
+    gca().set_aspect('equal', adjustable='box')
+    xlabel('$x$')
+    ylabel('$y$')
+    title('Nozzle')
+    savefig('images/nozzle.png', format='png')
+    close('all')
+
+plot_nozzle()
